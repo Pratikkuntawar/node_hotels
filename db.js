e32112db9ede5fa9abb11963d6fe4460493d0ec9
Your Name <you@example.com>
2024-06-03 09:45:33 +0530

    third version of project

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 //const mongoURl=process.env.MONGODB_URL_LOCAL//'mongodb://localhost:27017/hotels'
 // const mongoURl='mongodb+srv://kuntawarpratik4:pratik123@cluster0.hy1v36i.mongodb.net/'
 const mongoURL=process.env.mongoURL;
-mongoose.connect(mongoURl,{
+mongoose.connect(mongoURL,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 })
